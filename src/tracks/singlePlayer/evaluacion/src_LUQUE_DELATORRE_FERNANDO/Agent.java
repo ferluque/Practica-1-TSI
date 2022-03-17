2cebec87af2799343b71ab950af5852c95d4c0f7
ferluque <fl1562001@go.ugr.es>
2022-03-17 14:49:23 +0100

    update

--- src/tracks/singlePlayer/evaluacion/src_LUQUE_DELATORRE_FERNANDO/Agent.java
+++ src/tracks/singlePlayer/evaluacion/src_LUQUE_DELATORRE_FERNANDO/Agent.java
@@ -5,16 +5,28 @@ import core.player.AbstractPlayer;
 import ontology.Types;
 import ontology.Types.ACTIONS;
 import tools.ElapsedCpuTimer;
+import tools.Vector2d;
+import java.util.Queue;
+import java.util.LinkedList;
+
+import java.util.ArrayList;
 
 public class Agent extends AbstractPlayer{
 
-
+	private ArrayList<Types.ACTIONS> current_node;
+	private Queue<ArrayList<Types.ACTIONS>> nodes_queue;
+	private Vector2d myPos;
+	
+	
 	/**
 	 * initialize all variables for the agent
 	 * @param stateObs Observation of the current state.
      * @param elapsedTimer Timer when the action returned is due.
 	 */
 	public Agent(StateObservation stateObs, ElapsedCpuTimer elapsedTimer){
+		myPos = stateObs.getAvatarPosition();
+		nodes_queue = new LinkedList<ArrayList<Types.ACTIONS>>();
+		current_node = new ArrayList<Types.ACTIONS>();
 	}
 	
 	/**
@@ -25,8 +37,41 @@ public class Agent extends AbstractPlayer{
 	 */
 	@Override
 	public ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
-		System.out.println("Hola");
-		return Types.ACTIONS.ACTION_NIL;
+		
+		// Extraemos el nodo a expandir de la cola
+		current_node = nodes_queue.remove();
+		
+		// Vemos a qué posiciones nos podemos mover		
+		ArrayList<Types.ACTIONS> available = stateObs.getAvailableActions();
+		
+		// Guardamos la acción a tomar ahora que será la última de las que llevemos tomadas
+		Types.ACTIONS ret = current_node.get(current_node.size()-1);
+		
+		if (available.contains(Types.ACTIONS.ACTION_UP)) {
+			// Creamos el nuevo nodo como copia del actual
+			ArrayList<Types.ACTIONS> new_node = new ArrayList<>(current_node);
+			// Le añadimos la siguiente decisión
+			new_node.add(Types.ACTIONS.ACTION_UP);
+			// Lo añadimos a la cola de nodos
+			nodes_queue.add(new_node);
+		}
+		if (available.contains(Types.ACTIONS.ACTION_DOWN)) {
+			ArrayList<Types.ACTIONS> new_node = new ArrayList<>(current_node);
+			new_node.add(Types.ACTIONS.ACTION_DOWN);
+			nodes_queue.add(new_node);
+		}
+		if (available.contains(Types.ACTIONS.ACTION_LEFT)) {
+			ArrayList<Types.ACTIONS> new_node = new ArrayList<>(current_node);
+			new_node.add(Types.ACTIONS.ACTION_LEFT);
+			nodes_queue.add(new_node);
+		}
+		if (available.contains(Types.ACTIONS.ACTION_RIGHT)) {
+			ArrayList<Types.ACTIONS> new_node = new ArrayList<>(current_node);
+			new_node.add(Types.ACTIONS.ACTION_RIGHT);
+			nodes_queue.add(new_node);
+		}
+		
+		return ret;
 		
 	}
 }
