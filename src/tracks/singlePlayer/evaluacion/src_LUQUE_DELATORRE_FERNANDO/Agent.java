9e2445bb36e0a9cb3833dae2ef939534d06a1584
ferluque <fl1562001@go.ugr.es>
2022-03-23 20:59:22 +0100

    BFS falta saber a qué posiciones me puedo mover sin hacerlo muy ineficiente

--- src/tracks/singlePlayer/evaluacion/src_LUQUE_DELATORRE_FERNANDO/Agent.java
+++ src/tracks/singlePlayer/evaluacion/src_LUQUE_DELATORRE_FERNANDO/Agent.java
@@ -6,16 +6,22 @@ import ontology.Types;
 import ontology.Types.ACTIONS;
 import tools.ElapsedCpuTimer;
 import tools.Vector2d;
+import core.game.Observation;
 
-import java.util.ArrayList;
+import java.util.*;
 
 
 
 public class Agent extends AbstractPlayer{
 
 	boolean think = true;
-	int pos_inicial;
-	float fescala;
+	Vector2d fescala;
+	
+	Node inicio, fin;
+	
+	Queue<Node> toExpand = new LinkedList<Node>();
+	Queue<ACTIONS> decisiones = new LinkedList<>();
+	Stack<Node> camino = new Stack<>();
 	
 	
 	/**
@@ -24,25 +30,101 @@ public class Agent extends AbstractPlayer{
      * @param elapsedTimer Timer when the action returned is due.
 	 */
 	public Agent(StateObservation stateObs, ElapsedCpuTimer elapsedTimer){
-		fescala = stateObs.getWorldDimension().height
-	}
-	
-	private void plan() {
+		fescala = new Vector2d(stateObs.getWorldDimension().width / stateObs.getObservationGrid().length,
+				stateObs.getWorldDimension().height / stateObs.getObservationGrid()[0].length);
 		
+		// Almacenamos la posición de los portales convirtiendo de coordenadas píxel a casilla
+		ArrayList<Observation>[] posiciones = stateObs.getPortalsPositions();
+		Vector2d portal = posiciones[0].get(0).position;
+		
+		fin = new Node();
+		fin.row = (int)Math.floor(portal.y / fescala.y);
+		fin.column = (int)Math.floor(portal.x / fescala.x);
+		
+		// Hacemos lo mismo con la posición inicial
+		inicio = new Node();
+		Vector2d posJugador = stateObs.getAvatarPosition();
+		inicio.row = (int)Math.floor(posJugador.y/fescala.y);
+		inicio.column = (int)Math.floor(posJugador.x/fescala.x);
+		
+		System.out.println("Estoy creado");
 	}
 	
-	private int posToId (Vector2d pos, int ancho_mapa) {
-		// Tenemos que dividir entre 50.0 para obtener la posición en el grid	
-		int row = (int)pos.y/50;
-		int column = (int)pos.x/50;
-//		System.out.println(row+ ", "+column);
-//		System.out.println(ancho_mapa);
-		return row*ancho_mapa+column;
+	private ArrayList<Node> getSucesores(StateObservation stateObs, Node u) {
+		ArrayList<Node> sucesores = new ArrayList<>();
+		// Me estaba devolviendo todas las direcciones no solo las posibles
+		
+		ArrayList<Observation>[] moviles = stateObs.getMovablePositions();
+		
+		
+		
+		for (ACTIONS a: posiblesAcciones) {
+			switch (a) {
+				case ACTION_UP:sucesores.add(new Node(u.row+1, u.column));break;
+				case ACTION_DOWN:sucesores.add(new Node(u.row-1, u.column));break;
+				case ACTION_LEFT:sucesores.add(new Node(u.row, u.column-1));break;
+				case ACTION_RIGHT:sucesores.add(new Node(u.row, u.column+1));break;
+				default:break;
+			}		
+		}
+		return sucesores;		
 	}
 	
-	private Vector2d IdtoPos (int id, int ancho_mapa) {
-		Vector2d pos = new Vector2d(id%ancho_mapa, id/ancho_mapa);
-		return pos;
+	private void plan(StateObservation stateObs) {
+		think = false;
+		fescala = new Vector2d(stateObs.getWorldDimension().width / stateObs.getObservationGrid().length,
+				stateObs.getWorldDimension().height / stateObs.getObservationGrid()[0].length);
+		
+		// Implementamos la búsqueda en profundidad
+		inicio.visited = true;
+		inicio.parent = null;
+		toExpand.offer(inicio);
+		Node u=inicio;
+		while (!toExpand.isEmpty()) {
+			u = toExpand.poll();
+			if (u==fin) 
+				break;
+			else {
+				ArrayList<Node> sucesores = getSucesores(stateObs, u);
+				for (Node v: sucesores) {
+					if (!v.visited) {
+						v.visited = true;
+						v.parent = u;
+						toExpand.offer(v);
+					}
+				}
+			}
+		}
+		// Recorremos el camino de vuelta almacenando los nodos
+		Node actual = u;
+		while (actual != inicio) {
+			camino.push(actual);
+			actual = u.parent;
+		}
+		camino.push(inicio);
+		
+		Node siguiente = new Node();
+		// Una vez conocidos los nodos averiguamos la secuencia de acciones
+		while (!camino.isEmpty()) {
+			actual = camino.pop();
+			siguiente = camino.pop();
+			
+			// Separamos movimientos horizontales de verticales
+			// Horizontales (no se mueve en la columna
+			if (actual.row == siguiente.row) {
+				// Ha avanzado a la derecha
+				if (actual.column < siguiente.column)
+					decisiones.offer(ACTIONS.ACTION_RIGHT);
+				else
+					decisiones.offer(ACTIONS.ACTION_LEFT);
+			}
+			else {
+				if (actual.row < siguiente.row)
+					decisiones.offer(ACTIONS.ACTION_UP);
+				else
+					decisiones.offer(ACTIONS.ACTION_DOWN);
+			}
+		}
 	}
 	
 	/**
@@ -53,13 +135,16 @@ public class Agent extends AbstractPlayer{
 	 */
 	@Override
 	public ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
-//		if (think)
-//			plan();
+		System.out.println("Estoy pensando");
+		if (think)
+			plan(stateObs);
 //		int row = (int)stateObs.getAvatarPosition().y/50;
 //		int column = (int)stateObs.getAvatarPosition().x/50;
 //		System.out.println(row+ ", "+column);
 		
-		return ACTIONS.ACTION_LEFT;
+		System.out.println("Hola");
+		
+		return decisiones.poll();
 	}
 }
 
--- src/tracks/singlePlayer/evaluacion/src_LUQUE_DELATORRE_FERNANDO/Node.java
+++ src/tracks/singlePlayer/evaluacion/src_LUQUE_DELATORRE_FERNANDO/Node.java
@@ -3,8 +3,19 @@ package tracks.singlePlayer.evaluacion.src_LUQUE_DELATORRE_FERNANDO;
 class Node {
 	int row, column;
 	Node parent;
-		
-	public void setParent(Node p) {
-		parent = p;
+	boolean visited;
+	
+	Node() {
+		visited = false;
+		parent = null;
+		row = -1;
+		column = -1;
+	}
+	
+	Node(int r, int c) {
+		row = r;
+		column = c;
+		visited = false;
+		parent = null;
 	}
 }
